669529f3248525312aebd728287918584f0d0960
Leonardo Fabretti <leonardofabretti3@gmail.com>
2026-02-19 21:21:39 -0300

    Update register.v2.js

--- public/js/register.v2.js
+++ public/js/register.v2.js
@@ -18,7 +18,7 @@
    =================================== */
 
 const CONFIG = {
-  API_URL: '/api/auth/register',
+  API_URL: 'https://empowering-solace-production-c913.up.railway.app/api/auth/register',
   MIN_NAME_LENGTH: 3,
   MIN_PASSWORD_LENGTH: 8,
 };
